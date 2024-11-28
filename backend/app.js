61084022a59671dc1570043859e173511e12df67
AlwinRP <111759014+AlwinRP@users.noreply.github.com>
2024-11-28 11:46:07 +0530

    Update(cors update) app.js

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,8 @@ mongoose.connect(process.env.MONGO_URI , {
 .catch((err) => console.error('Could not connect to MongoDB',err));
 
 const corsOptions = {
-    origin: ['http://localhost:3000', 'https://rbac-client.vercel.app/'], // Replace this with your frontend URL
+    origin: ['http://localhost:3000', 'https://rbac-client.vercel.app/'],// Replace this with your frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
     optionsSuccessStatus: 200
 };
 
